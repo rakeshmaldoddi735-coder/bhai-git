e47947a596ba47ccc3a0e8991bd3cf7373813ab4
rakesh <rakeshmaldoddi75@gmail.com>
2026-04-14 09:45:03 +0530

    added documnets

--- script4.js
+++ script4.js
@@ -3,3 +3,15 @@ let name="bhai";
 let age="23"
 console.log("my...name is "+ name +" .he is "+age);
 console.log(`my... name is ${name}.he is ${age}`);
+//console.log(document);
+//console.log(document.body);
+//console.log(buchi.innerText);
+//let buchi=document.getElementById("buchi");
+//buchi.innerText = "<b>hello</b>";
+//buchi.innerHTML = "<b>hello</b>";
+let boxes = document.getElementsByClassName("box");
+let para = document.getElementsByClassName("para");
+boxes[1].innerHTML = "bye";
+boxes[2].innerHTML = "bye";
+
+para[0].innerHTML = "holaa";
