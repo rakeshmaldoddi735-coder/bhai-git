efdf3e96ede66c48333f7fa578a852bb4d32203c
rakesh <rakeshmaldoddi75@gmail.com>
2026-04-15 13:10:06 +0530

    adedd html,css,js

--- script5.js
+++ script5.js
@@ -1,8 +1,23 @@
 
-const myFun = () => {
-    console.log("bhai");
-    let a = document.getElementsByClassName("hello");
-    a.innerHTML="bye";
-    let b = document.getElementById("display");
+const myfun = () => {
+   // console.log("bhai");
+    //let a = document.getElementsByClassName("hello");
+    //a.innerHTML="bye";
+    let b = document.getElementById("disply");
     b.innerHTML = b.innerHTML === `hello`?`bye` : `hello`
+    b.style.color = b.style.color === `red`?`blue` : `red`
+
+};
+let isShow = true ;
+const myFun = () => {
+    let c = document.getElementById("display");
+  if(isShow)
+  {
+    c.src = "C:\\Users\\rocks\\OneDrive\\Pictures\\1707468592733.jpg"
+    isShow = !isShow
+  }
+  else{
+    c.src = "C:\\Users\\rocks\\OneDrive\\Pictures\\IMG_20241204_174632.jpg"
+ isShow =!isShow 
+}
 };
